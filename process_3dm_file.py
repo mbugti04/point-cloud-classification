fa32ff29b6572c4b3a281bf6fc0501468a54a274
mbugti04 <mustafammbg@gmail.com>
2024-12-19 12:14:34 -0600

    able to load vertices to certain point threshold and save/load from json

--- process_3dm_file.py
+++ process_3dm_file.py
@@ -1,37 +1,73 @@
-import trimesh
+import json
 import matplotlib.pyplot as plt
 import numpy as np
 import rhino3dm
 
-def load():
+def load_model(file_name):
+     return rhino3dm.File3dm.Read(file_name)
+
+def process_3dm(model):
     combined_vertices = []
-    # get file NYC_3DModel_MN01.3dm from local folder
-    model = rhino3dm.File3dm.Read("NYC_3DModel_MN01.3dm")
 
     for obj in model.Objects:
-            geometry = obj.Geometry
-            if isinstance(geometry, rhino3dm.Brep):
-                brep = geometry
-                # face = brep.Faces[0]
+        geometry = obj.Geometry
+        if isinstance(geometry, rhino3dm.Brep):
+            brep = geometry
+            faces = brep.Faces
+            
+            for face in faces:
                 mesh = face.GetMesh(rhino3dm.MeshType.Any)
-
-                # print (len(mesh.Faces))
-                # print(mesh)
                 vertices = [[vertex.X, vertex.Y, vertex.Z] for vertex in mesh.Vertices]
-                # print(vertices)
-                # faces = [face for face in mesh.Faces]
-                # trimesh_mesh = trimesh.Trimesh(vertices=vertices)
-
-                # for vertex in vertices:
-                #     combined_vertices.append(vertex)
-
-                print(brep)
-
-                # break
-                # return trimesh_mesh
-
-                break
+                for vertex in vertices:
+                    combined_vertices.append(vertex)
+        # if isinstance(geometry, rhino3dm.PolylineCurve):
+        #     polylinecurve = geometry
+        #     point_count = polylinecurve.PointCount
+        #     for i in range(point_count):
+        #         point = polylinecurve.Point(i)
+        #         # print(point.X, point.Y, point.Z)
+        #         combined_vertices.append([point.X, point.Y, point.Z]
 
     return combined_vertices
 
-load()
+def save_to_json(vertices, file_name):
+    vertices = vertices.tolist()
+    with open(file_name, 'w') as f:
+        json.dump(vertices, f)
+
+def load_from_json(file_name):
+    try:
+        with open(file_name, 'r') as f:
+            vertices = json.load(f)
+        return vertices
+    except FileNotFoundError:
+        return False
+
+if __name__ == "__main__":
+    file_directory = r"data\cities"
+    file_name = r"\NYC_3DModel_MN05"
+
+    if not load_from_json(file_directory + file_name + ".json"):
+        print("Loading vertices from 3dm file")
+
+        model = load_model(file_directory + file_name + ".3dm")
+
+        vertices = process_3dm(model)
+        vertices = np.array(vertices)
+
+        print("Vertex count: ", len(vertices))
+
+        # Plot every x vertices
+        x = 100
+        vertices = vertices[::x]
+        print("Vertex count: ", len(vertices))
+        save_to_json(vertices, file_directory + file_name + ".json")
+    else:
+        print("Loading vertices from file")
+        vertices = np.array(load_from_json(file_directory + file_name + ".json"))
+
+    # plot vertices in 3D
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(vertices[:,0], vertices[:,1], vertices[:,2], s=1)
+    plt.show()
