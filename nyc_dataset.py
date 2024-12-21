4be7f15bada277d845cbafc858d9b5599155a4ce
mbugti04 <mustafammbg@gmail.com>
2024-12-21 01:04:15 -0600

    clean up dataset

--- nyc_dataset.py
+++ nyc_dataset.py
@@ -15,26 +15,14 @@ class NYCDataset(Dataset):
         return len(self.data_list)
 
     def get(self, idx):
-        # Implement your data processing logic here
-        # You can access the data from self.data_list[idx]
-        # and convert it into a PyTorch Geometric Data object
-
-        # Example:
         data = self.data_list[idx]
-        # Process the data and convert it into a PyTorch Geometric Data object
-        # ...
 
         return data
 
 if __name__ == "__main__":
-    # Assuming you have a data_list containing your data
     data_list = process_3dm_file.process_models()
-
-    # Create an instance of your dataset
     dataset = NYCDataset(data_list)
 
-    # Access the data using dataset[idx]
-    # Example:
     data = dataset[0]
 
     print(data)
